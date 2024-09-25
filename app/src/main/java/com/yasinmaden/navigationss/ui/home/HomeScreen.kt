0afd10552fe220f848bfd9502c9c9b1bb366e2c5
Yasin Maden <maden.yasin@hotmail.com>
2024-09-25 23:57:22 +0300

    ChooseBrandSection için basit bir ui yapıldı.

--- app/src/main/java/com/yasinmaden/navigationss/ui/home/HomeScreen.kt
+++ app/src/main/java/com/yasinmaden/navigationss/ui/home/HomeScreen.kt
@@ -1,14 +1,23 @@
 package com.yasinmaden.navigationss.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -27,6 +36,8 @@ import androidx.navigation.NavHostController
 import com.yasinmaden.navigationss.R
 import com.yasinmaden.navigationss.ui.components.EmptyScreen
 import com.yasinmaden.navigationss.ui.components.LoadingBar
+import com.yasinmaden.navigationss.ui.theme.Black
+import com.yasinmaden.navigationss.ui.theme.BrandCardContainerColor
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
@@ -63,12 +74,16 @@ fun HomeContent(
     uiState: HomeContract.UiState
 ) {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopStart
-    ) {
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.TopStart,
+
+        ) {
         Column {
             WelcomeSection()
             SearchBar()
+            ChooseBrandSection(brands = listOf("Nike", "Adidas", "Puma", "Reebok", "Under Armour"))
         }
 
 
@@ -76,27 +91,62 @@ fun HomeContent(
 }
 
 @Composable
-fun ChooseBrandSection() {
+fun ChooseBrandSection(brands: List<String>) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Choose Brand",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
 
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(brands) { brand ->
+                BrandCard(brandName = brand)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChooseBrandSectionPreview() {
+    ChooseBrandSection(brands = listOf("Nike", "Adidas", "Puma", "Reebok", "Under Armour"))
 }
 
 @Composable
 fun BrandCard(brandName: String) {
-    Card {
-        OutlinedButton(
-            onClick = { /* Handle button click */ },
-            modifier = Modifier.size(115.dp, 50.dp),
+    Card(
+        modifier = Modifier
+            .height(50.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
         ) {
-            Text(brandName)
+            Text(
+                text = brandName,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
 
-@Composable
 @Preview
+@Composable
 fun BrandCardPreview() {
-    BrandCard("Adidas")
+    BrandCard(brandName = "Sample Brand")
 }
+
+
 
 @Composable
 fun WelcomeSection() {
@@ -115,9 +165,11 @@ fun SearchBar() {
     OutlinedTextField(
         value = "",
         onValueChange = {},
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 0.dp, top = 0.dp, start = 16.dp, end = 16.dp),
         placeholder = {
-            Row(verticalAlignment = Alignment.CenterVertically){
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.ic_search),
                     contentDescription = "Search"
@@ -129,7 +181,6 @@ fun SearchBar() {
         }
     )
 }
-
 
 
 @Composable
