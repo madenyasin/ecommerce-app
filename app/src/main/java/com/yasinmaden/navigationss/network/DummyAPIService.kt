9fcadfbff8230817e9f55749e92fc6b4f3d428ac
Yasin Maden <maden.yasin@hotmail.com>
2024-09-27 00:13:45 +0300

    getProducts fonksiyonuna limit paremetresi eklendi.

--- app/src/main/java/com/yasinmaden/navigationss/network/DummyAPIService.kt
+++ app/src/main/java/com/yasinmaden/navigationss/network/DummyAPIService.kt
@@ -1,11 +1,12 @@
-package com.yasinmaden.navigationss.data.network
+package com.yasinmaden.navigationss.network
 
 import com.yasinmaden.navigationss.data.model.product.ProductResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface DummyAPIService {
     @GET("products")
-    suspend fun getProducts(): ProductResponse
+    suspend fun getProducts(@Query("limit") limit: Int = 0): ProductResponse
 
     @GET("products/category-list")
     suspend fun getCategories(): List<String>
