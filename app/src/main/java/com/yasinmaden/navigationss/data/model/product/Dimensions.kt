adafe45dd9a3dc2e5ad49d79b524f2c2e4a6c658
Yasin Maden <maden.yasin@hotmail.com>
2024-09-26 03:29:51 +0300

    optimize

--- app/src/main/java/com/yasinmaden/navigationss/data/model/product/Dimensions.kt
+++
@@ -1,7 +0,0 @@
-package com.yasinmaden.navigationss.data.model.product
-
-data class Dimensions(
-    val depth: Double,
-    val height: Double,
-    val width: Double
-)
--- app/src/main/java/com/yasinmaden/navigationss/data/model/product/Meta.kt
+++
@@ -1,8 +0,0 @@
-package com.yasinmaden.navigationss.data.model.product
-
-data class Meta(
-    val barcode: String,
-    val createdAt: String,
-    val qrCode: String,
-    val updatedAt: String
-)
